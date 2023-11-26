ac80a76dd9a91272d457c0aca4e5222ea9a889f8
kayra09 <gskayra322@gmail.com>
2023-11-26 17:41:07 +0300

    ata güler
    Library Meeting Initilazation of the project, Database testing commit

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 from flask import *
+import mysql.connector
+
 
 app = Flask(__name__)
 app.secret_key = "not so secret key"
 
-
+def database_test():
+    db = mysql.connector.connect(host='localhost', database='capstone_project', user='root', password='root')
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM course_code")
+    records=cursor.fetchall()
+    for data in records:
+        print(data)
 @app.route("/")
 def main_page():
     return render_template("index.html")
 
 if __name__ == '__main__':
+    database_test()
     app.run()
