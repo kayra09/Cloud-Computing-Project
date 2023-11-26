453feca0c326ef6397e65287bd799f53b08215a5
kayra09 <gskayra322@gmail.com>
2023-11-26 17:57:53 +0300

    kayra
    main page connection between index and selected course page for materials
    (part of library meeting)

--- main.py
+++ main.py
@@ -5,17 +5,19 @@ import mysql.connector
 app = Flask(__name__)
 app.secret_key = "not so secret key"
 
-def database_test():
+def get_course_table():
     db = mysql.connector.connect(host='localhost', database='capstone_project', user='root', password='root')
     cursor = db.cursor()
     cursor.execute("SELECT * FROM course_code")
-    records=cursor.fetchall()
-    for data in records:
-        print(data)
+    course_codes=cursor.fetchall()
+    return course_codes
+
 @app.route("/")
 def main_page():
-    return render_template("index.html")
-
+    course_codes = get_course_table()
+    return render_template("index.html",course_codes=course_codes)
+@app.route("/see_course/<course_code>")
+def see_course(course_code):
+    return render_template("see_course.html",course_code=course_code)
 if __name__ == '__main__':
-    database_test()
     app.run()
